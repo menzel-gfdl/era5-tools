b02a82739038f3928d4d169284a8256010cc2f3b
Raymond Menzel <Raymond.Menzel@noaa.gov>
2020-02-12 11:09:29 -0500

    remove negatives caused by era5 scale_factor and add_offset values

--- era5/main.py
+++ era5/main.py
@@ -3,20 +3,20 @@ from argparse import ArgumentParser
 from .combine import Combine
 from .download import Download
 from .horizontal_remap import HorizontalRemap
+from .remove_negatives import RemoveNegatives
 from .vertical_remap import VerticalRemap
 
 
 def main():
     parser = ArgumentParser(description="ERA5 reanalysis radiation tool.")
     subparsers = parser.add_subparsers(title="commands", help="command help")
-
     Combine().arguments(subparsers)
     Download().arguments(subparsers)
     HorizontalRemap().arguments(subparsers)
+    RemoveNegatives().arguments(subparsers)
     VerticalRemap().arguments(subparsers)
     args = parser.parse_args()
-#   try:
-    args.func(args)
-#   except AttributeError as e:
-#       print(str(e))
-#       parser.error("Please specify a valid command.")
+    try:
+        args.func(args)
+    except AttributeError:
+        parser.error("Please specify a valid command.")
---
+++ era5/remove_negatives.py
@@ -0,0 +1,42 @@
+"""Removes negative values from variables in a netCDF dataset."""
+from netCDF4 import Dataset
+from numpy import argwhere
+
+from .commands import Command
+
+
+class RemoveNegatives(Command):
+    @staticmethod
+    def arguments(subparsers):
+        """Adds command specific command-line arguments.
+
+        Args:
+            subparsers: Special action object returned by ArgumentParser().add_subparsers().
+        """
+        command = "remove-negatives"
+        parser = subparsers.add_parser(command, help="{} help.".format(command))
+        parser.add_argument("dataset", help="Dataset to be modified.")
+        parser.set_defaults(func=remove_negatives)
+
+
+def remove_negatives(args):
+    """Replaces negative values with a small positive value.
+
+    Args:
+        args: Namespace object returned by ArgumentParser().parse_args().
+    """
+    blacklist = ["msdwlwrfcs", "msdwswrfcs", "msnlwrfcs", "msnswrfcs",
+                 "mtdwswrf", "mtnlwrfcs", "mtnswrfcs"]
+    with Dataset(args.dataset, "a") as dataset:
+        blacklist += [x for x in dataset.dimensions.keys()]
+        for name, v in dataset.variables.items():
+            if name in blacklist:
+                continue
+            data = v[...]
+            indices = argwhere(data < 0.)
+            if indices.shape[0] == 0:
+                continue
+            scale, offset = v.getncattr("scale_factor"), v.getncattr("add_offset")
+            smallest_positive = scale*(v.datatype.type(-1.*offset/scale) + 1) + offset
+            for i in range(indices.shape[0]):
+                v[tuple(indices[i,...])] = smallest_positive
